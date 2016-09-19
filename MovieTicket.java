23f3012dbdaf120a1c4ce2b3cd269ed914ff5abe
Twilight <twilightdragon282@gmail.com>
2016-09-18 21:04:02 -0500

    Create MovieTicket.java

---
+++ MovieTicket.java
@@ -0,0 +1,26 @@
+public MovieTicket{
+
+  private DateTime dateTime;
+  private String name;
+  private int screen;
+
+
+  public MovieTicket(DateTime dateTime,String name,int screen){
+    this.dateTime = dateTime;
+    this.name = name;
+    this.screen = screen;
+  }
+
+  public DateTime getDateTime(){
+    return this.dateTime;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getScreen(){
+    return this.screen;
+  }
+
+}
