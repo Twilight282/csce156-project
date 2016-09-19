ff55a67957db3a192db2846a4b208da556817a5a
Twilight <twilightdragon282@gmail.com>
2016-09-18 21:33:51 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+public Student extends Customer{
+
+  private String contact;
+
+  public Student(String code, String name, Address address, String contact){
+    this.contact = contact;
+    super(code, name, address);
+  }
+
+  public String getContact(){
+    return this.contact;
+  }
+
+}
