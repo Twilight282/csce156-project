b2cbbc12cbd1c22b62dc2c759acb4d2c8557f3e5
Twilight <twilightdragon282@gmail.com>
2016-09-18 21:14:35 -0500

    Create SeasonPass.java

---
+++ SeasonPass.java
@@ -0,0 +1,26 @@
+public SeasonPass{
+
+  private DateTime startDate;
+  private DateTime endDate;
+  private DateTime effectiveDate;
+
+
+  public SeasonPass(DateTime startDate,DateTime endDate,DateTime effectiveDate){
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.effectiveDate = effectiveDate;
+  }
+
+  public DateTime getStartDate(){
+    return this.startDate;
+  }
+
+  public DateTime getEndDate(){
+    return this.endDate;
+  }
+
+  public DateTime getEffectiveDate(){
+    return this.effectiveDate;
+  }
+
+}
