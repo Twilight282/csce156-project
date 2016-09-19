b7fbc7dde7676f9f2da9da651fa516f22993ca61
Twilight <twilightdragon282@gmail.com>
2016-09-18 22:24:17 -0500

    Create Invoice.java

---
+++ Invoice.java
@@ -0,0 +1,31 @@
+public class Invoice{
+  
+  private String code;
+  private Customer buyer;
+  private Employee seller;
+  private ArrayList<Product> cart;
+  
+  public Invoice(String code, Customer buyer, Employee seller, ArrayList<Product> cart){
+    this.code = code;
+    this.buyer = buyer;
+    this.seller = seller;
+    this.cart = cart;
+  }
+  
+  public String getCode(){
+    return this.code;
+  }
+  
+  public Customer getBuyer(){
+    return this.buyer;
+  }
+  
+  public Employee getSeller(){
+    return this.seller;
+  }
+  
+  public ArrayList<Product> getCart(){
+    return this.cart;
+  }
+  
+}
