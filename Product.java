34319273effeaf7e92d652ba49a7dd1c9ce247d8
Twilight <twilightdragon282@gmail.com>
2016-09-18 21:12:44 -0500

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,14 +1,19 @@
 public Product{
 
   private double price;
+  private String code;
 
-
-  public Product(double price){
+  public Product(double price, String code){
     this.price = price;
+    this.code = code;
   }
 
   public double getPrice(){
     return this.price;
+  }
+  
+  public String getCode(){
+    return this.code;
   }
 
 }
