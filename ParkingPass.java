401141b3a269bf065ff18450d5e1b8f93cc43d04
Twilight <twilightdragon282@gmail.com>
2016-09-20 12:22:04 -0500

    Update ParkingPass.java

--- ParkingPass.java
+++ ParkingPass.java
@@ -3,13 +3,13 @@ public class ParkingPass extends Service{
   private boolean discounted;
 
 
-  public ParkingPass(String code, double price, boolean discounted){
+  public ParkingPass(String code, double price){
     super(code, price);
-    this.discounted = discounted;
+    this.discounted = false;
   }
   
   public void discount(){
-    this.discounted = True;
+    this.discounted = true;
   }
 
   public boolean getDiscounted(){
