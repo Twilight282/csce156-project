0e1af9873d235ca0f350006fdb58ca506632d6a8
Twilight <twilightdragon282@gmail.com>
2016-09-20 11:41:44 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,7 +1,14 @@
 public class Customer extends People{
 
+  protected String name;
+
   public Customer(String code, String name, Address address){
-    super(code, name, address);
+    super(code, address);
+    this.name = name;
+  }
+  
+  public String getName(){
+    return this.name;
   }
   
 }
