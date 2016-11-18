354cdf259e3c6a9c50c15cdb1a35fc18cf6c6127
kcinesser <ngesser11@gmail.com>
2016-11-18 10:09:12 -0600

    Add files via upload

--- InvoiceData.java
+++ InvoiceData.java
@@ -50,20 +50,37 @@ public class InvoiceData {
 	}
 
 	
-	
+	public static void test() {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+		
+		String test = "INSERT INTO Persons values (1234,1234,1234,1234,1234,1231,123,1234);";
+		
+		try {
+			ps = conn.prepareStatement(test);
+			ps.executeUpdate();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 	/**
 	 * 1. Method that removes every person record from the database
 	 */
 	public static void removeAllPersons() {
 		Connection conn = InvoiceData.getConnection();
 		PreparedStatement ps;
-		ResultSet rs;
 		
-		String removePerson = "DELETE * FROM Persons;";
+		String removePerson = "DELETE FROM Persons;";
 		
 		try {
 			ps = conn.prepareStatement(removePerson);
-			ps.executeQuery();
+			ps.executeUpdate();
+			ps.close();
 		} 
 		catch (SQLException e) 
 		{
@@ -85,7 +102,33 @@ public class InvoiceData {
 	 * @param zip
 	 * @param country
 	 */
-	public static void addPerson(String personCode, String firstName, String lastName, String street, String city, String state, String zip, String country) {}
+	public static void addPerson(String personCode, String firstName, String lastName, String street, String city, String state, String zip, String country) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addPerson = "INSERT INTO Persons (PersonCode, PersonName, Street, City, State, Zip, Country) values (?, ?, ?, ?, ?, ?, ?);";
+			
+			String name = lastName + ", " + firstName;  
+			
+			ps = conn.prepareStatement(addPerson);
+			ps.setString(1, personCode);
+			ps.setString(2, name);
+			ps.setString(3, street);
+			ps.setString(4, city);
+			ps.setString(5, state);
+			ps.setString(6, zip);
+			ps.setString(7, country);
+			ps.executeQuery();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
 	/**
 	 * 3. Adds an email record corresponding person record corresponding to the
@@ -94,49 +137,265 @@ public class InvoiceData {
 	 * @param personCode
 	 * @param email
 	 */
-	public static void addEmail(String personCode, String email) {}
+	public static void addEmail(String personCode, String email) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addEmail = "INSERT INTO Emails (Email, PersonCode) values (?, ?);";
+			
+			int personID = Integer.parseInt(personCode);
+			
+			ps = conn.prepareStatement(addEmail);
+			ps.setString(1, email);
+			ps.setString(2, personCode);
+			ps.executeQuery();
+			ps.close();
+			
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
 
 	/**
 	 * 4. Method that removes every customer record from the database
 	 */
-	public static void removeAllCustomers() {}
+	public static void removeAllCustomers() {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+		
+		String removeCustomers = "DELETE * FROM Customers;";
+		
+		try {
+			ps = conn.prepareStatement(removeCustomers);
+			ps.executeUpdate();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
-	public static void addCustomer(String customerCode, String customerType, String primaryContactPersonCode,String name, String street, String city, String state, String zip, String country) {}
+	public static void addCustomer(String customerCode, String customerType, String primaryContactPersonCode,String name, String street, String city, String state, String zip, String country) {
+		try {
+			Connection conn = InvoiceData.getConnection();
+			PreparedStatement ps;
+			
+			String addCustomer = "INSERT INTO Persons (CustomerCode, CustomerName, ContactID, CustomerType, Street, City, State, Zip, Country) values (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+			int custType = Integer.parseInt(customerType);
+			
+			ps = conn.prepareStatement(addCustomer);
+			ps.setString(1, customerCode);
+			ps.setString(2, name);
+			ps.setString(3, primaryContactPersonCode);
+			ps.setInt(4, custType);
+			ps.setString(5, street);
+			ps.setString(6, city);
+			ps.setString(7, state);
+			ps.setString(8, zip);
+			ps.setString(9, country);
+			ps.executeQuery();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 	
 	/**
 	 * 5. Removes all product records from the database
 	 */
-	public static void removeAllProducts() {}
+	public static void removeAllProducts() {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+		
+		String removeProducts = "DELETE * FROM Products;";
+		
+		try {
+			ps = conn.prepareStatement(removeProducts);
+			ps.executeQuery();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
 	/**
 	 * 6. Adds an movieTicket record to the database with the provided data.
 	 */
-	public static void addMovieTicket(String productCode, String dateTime, String movieName, String street, String city,String state, String zip, String country, String screenNo, double pricePerUnit) {}
+	public static void addMovieTicket(String productCode, String dateTime, String movieName, String street, String city,String state, String zip, String country, String screenNo, double pricePerUnit) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addTicket = "INSERT INTO MovieTickets (ProductCode, MovieName, Price, Date, Street, City, State, Zip, Country, ScreenNumber) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+			
+			ps = conn.prepareStatement(addTicket);
+			ps.setString(1, productCode);
+			ps.setString(2, movieName);
+			ps.setDouble(3, pricePerUnit);
+			ps.setString(4, dateTime);
+			ps.setString(6, street);
+			ps.setString(6, city);
+			ps.setString(7, state);
+			ps.setString(8, zip);
+			ps.setString(9, country);
+			ps.setString(10, screenNo);
+			ps.executeQuery();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
 	/**
 	 * 7. Adds a seasonPass record to the database with the provided data.
 	 */
-	public static void addSeasonPass(String productCode, String name, String seasonStartDate, String seasonEndDate,	double cost) {}
+	public static void addSeasonPass(String productCode, String name, String seasonStartDate, String seasonEndDate,	double cost) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addPass = "INSERT INTO SeasonPasses (ProductCode, PassName, StartDate, EndDate, Price) values (?, ?, ?, ?, ?);";
+						
+			ps = conn.prepareStatement(addPass);
+			ps.setString(1, productCode);
+			ps.setString(2, name);
+			ps.setString(3, seasonStartDate);
+			ps.setString(4, seasonEndDate);
+			ps.setDouble(5, cost);
+			ps.executeQuery();
+			ps.close();
+			
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
 	/**
 	 * 8. Adds a ParkingPass record to the database with the provided data.
 	 */
-	public static void addParkingPass(String productCode, double parkingFee) {}
+	public static void addParkingPass(String productCode, double parkingFee) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addPass = "INSERT INTO ParkingPasses (ProductCode, Price) values (?, ?);";
+						
+			ps = conn.prepareStatement(addPass);
+			ps.setString(1, productCode);
+			ps.setDouble(2, parkingFee);
+			ps.executeQuery();
+			ps.close();
+			
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
 	/**
 	 * 9. Adds a refreshment record to the database with the provided data.
 	 */
-	public static void addRefreshment(String productCode, String name, double cost) {}
+	public static void addRefreshment(String productCode, String name, double cost) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addRefresh = "INSERT INTO Refreshemnts (ProductCode, RefreshmentName, Price) values (?, ?, ?);";
+						
+			ps = conn.prepareStatement(addRefresh);
+			ps.setString(1, productCode);
+			ps.setString(2, name);
+			ps.setDouble(3, cost);
+			ps.executeQuery();
+			ps.close();
+			
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
 
 	/**
 	 * 10. Removes all invoice records from the database
 	 */
-	public static void removeAllInvoices() {}
+	public static void removeAllInvoices() {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+		
+		String removeInvoices = "DELETE * FROM Invoices;";
+		
+		try {
+			ps = conn.prepareStatement(removeInvoices);
+			ps.executeQuery();
+			ps.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 
 	/**
 	 * 11. Adds an invoice record to the database with the given data.
 	 */
-	public static void addInvoice(String invoiceCode, String customerCode, String salesPersonCode, String invoiceDate) {}
+	public static void addInvoice(String invoiceCode, String customerCode, String salesPersonCode, String invoiceDate) {
+		Connection conn = InvoiceData.getConnection();
+		PreparedStatement ps;
+						
+		try {
+			String addEmail = "INSERT INTO Invoices (InvoiceCode, CustomerCode, PersonCode, Date) values (?, ?, ?, ?);";
+			
+			ps = conn.prepareStatement(addEmail);
+			ps.setString(1, invoiceCode);
+			ps.setString(2, customerCode);
+			ps.setString(3, salesPersonCode);
+			ps.setString(4, invoiceDate);
+			ps.executeQuery();
+			ps.close();
+			
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
 
 	/**
 	 * 12. Adds a particular movieticket (corresponding to <code>productCode</code>
@@ -144,7 +403,9 @@ public class InvoiceData {
 	 * the given number of units
 	 */
 
-	public static void addMovieTicketToInvoice(String invoiceCode, String productCode, int quantity) {}
+	public static void addMovieTicketToInvoice(String invoiceCode, String productCode, int quantity) {
+		
+	}
 
 	/*
 	 * 13. Adds a particular seasonpass (corresponding to <code>productCode</code>
